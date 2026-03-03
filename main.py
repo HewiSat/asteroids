289c0e53ab3a5f7aca8026e88ac3e3ada01c9de3
HewiSat <satasiah@gmail.com>
2026-03-03 06:39:53 +0000

    set the FPS to 60

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
     number = 0
     while number == 0:
         log_state()
@@ -16,6 +18,7 @@ def main():
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
