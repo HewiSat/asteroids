e44ae0fd71f5e730a419740f58388afb85c5b282
HewiSat <satasiah@gmail.com>
2026-03-08 20:06:38 +0000

    moving around

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     
@@ -17,13 +17,18 @@ def main():
 
     while number == 0:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
+        
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)/1000
+
+        dt = clock.tick(60) / 1000 #limit the framerate to 60 FPS
 
 
 if __name__ == "__main__":
